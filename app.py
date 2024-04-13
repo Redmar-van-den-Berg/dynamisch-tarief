0b62bc8e472d4c24c3a14fd4bc3bedeccb65db92
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-04-13 11:20:47 +0200

    Add endpoint to calculate hourly averages

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Body
-from typing import List
+from typing import List, Dict
+from collections import defaultdict
+from statistics import mean
 
 from models import HourlyPrice, data, DataSource, datetime
 
@@ -15,6 +17,7 @@ async def root():
 
 @app.get("/hourly/{source}/{date}")
 async def get_date(source: DataSource, date: datetime) -> HourlyPrice:
+    """Retrieve the price at the specified date"""
     return d.get(source, start=date)
 
 
@@ -22,4 +25,15 @@ async def get_date(source: DataSource, date: datetime) -> HourlyPrice:
 async def get_range(
     source: DataSource, start: datetime, end: datetime
 ) -> List[HourlyPrice]:
+    """Retrieve the prices in the specified range"""
     return list(d.range(source, start=start, end=end))
+
+@app.post("/average/{items}")
+async def calc_average(hourly_rates: List[HourlyPrice]) -> Dict[str, float]:
+    """Calculate the average hourly rate"""
+    d = defaultdict(list)
+
+    for item in hourly_rates:
+        d[item.date.hour].append(item.price)
+
+    return {str(hour): mean(d[hour]) for hour in d}
